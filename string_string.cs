1e61301738a466cef0106adf43c21d509aff7a40
carbonbear <chongsokcom@naver.com>
2018-05-10 13:23:01 +0900

    Update string_string.cs

--- string_string.cs
+++ string_string.cs
@@ -9,23 +9,8 @@ namespace 한글패치병합
     public class string_string
     {
         public string left;
-        public string Left{
-            get
-            {
-                return left;
-            }
-            set
-            {
-                left = value;
-            }
-        }
+        public string Left{ get{ return left; } set { left = value; } }
         public string right;
         public string Right{ get { return right; } set { right = value; } }
-
-        private void split(string oneline)
-        {
-
-
-        }
     }
 }
